9d501ba9c9277b925bceea7c231d20e7cb8c6c12
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-04-15 10:38:57 -0300

    fix. Ajustando taxa de juros

--- app/src/main/java/conta/App.java
+++ app/src/main/java/conta/App.java
@@ -1,5 +1,6 @@
 package conta;
 
+import conta.models.ContaPoupanca;
 import conta.models.Usuario;
 import conta.models.Conta;
 
@@ -8,24 +9,36 @@ public class App {
     public static void main(String[] args) {
 
         // TESTE CONTA CORRENTE
+//         Usuario usuario = new Usuario();
+//         usuario.criarConta("corrente", 0.0);
+//         Conta contaCorrente = usuario.getConta("corrente");
+//         System.out.println("Saldo inicial: " + contaCorrente.getSaldo());
+//         contaCorrente.depositar(500.0);
+//         System.out.println("Saldo após depósito de R$ 500.00: " +
+//         contaCorrente.getSaldo());
+//         contaCorrente.sacar(200.0);
+//         System.out.println("Saldo após saque de R$ 200.00: " +
+//         contaCorrente.getSaldo());
+//         try {
+//         contaCorrente.sacar(2000.0);
+//         } catch (RuntimeException ex) {
+//         System.out.println(ex.getMessage());
+//         }
+//         usuario.fecharConta("corrente");
+//         System.out.println("Conta corrente fechada com sucesso.");
 
-         Usuario usuario = new Usuario();
-         usuario.criarConta("corrente", 0.0);
-         Conta contaCorrente = usuario.getConta("corrente");
-         System.out.println("Saldo inicial: " + contaCorrente.getSaldo());
-         contaCorrente.depositar(500.0);
-         System.out.println("Saldo após depósito de R$ 500.00: " +
-         contaCorrente.getSaldo());
-         contaCorrente.sacar(200.0);
-         System.out.println("Saldo após saque de R$ 200.00: " +
-         contaCorrente.getSaldo());
-         try {
-         contaCorrente.sacar(2000.0);
-         } catch (RuntimeException ex) {
-         System.out.println(ex.getMessage());
-         }
-         usuario.fecharConta("corrente");
-         System.out.println("Conta corrente fechada com sucesso.");
-
+         // TESTE CONTA POUPANÇA
+          ContaPoupanca contaPoupanca = new ContaPoupanca(50.0);
+          System.out.println("Saldo após a abertura da conta: " +
+          contaPoupanca.getSaldo());
+          try {
+          contaPoupanca.sacar(30.0);
+          System.out.println("Saldo após o saque: " + contaPoupanca.getSaldo());
+          } catch (RuntimeException e) {
+          System.out.println("Erro ao tentar sacar: " + e.getMessage());
+          }
+          contaPoupanca.setDataDeAbertura(contaPoupanca.getDataDeAbertura().minusDays(1));
+          System.out.println("Saldo após um dia do depósito inicial: " +
+          contaPoupanca.getSaldo());
     }
 }
--- app/src/main/java/conta/models/ContaPoupanca.java
+++ app/src/main/java/conta/models/ContaPoupanca.java
@@ -1,14 +1,15 @@
 package conta.models;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 public class ContaPoupanca extends Conta {
-    private LocalDate dataDeAbertura;
+    private LocalDateTime dataDeAbertura;
 
     public ContaPoupanca(Double saldoInicial) {
         super(saldoInicial);
-        dataDeAbertura = LocalDate.now();
+        this.dataDeAbertura = LocalDateTime.now();
         if(saldoInicial <50.0){
             throw new RuntimeException("O depósito inicial deve ser de pelo menos R$ 50,00.");
         }
@@ -25,8 +26,15 @@ public class ContaPoupanca extends Conta {
 
     @Override
     public Double getSaldo() {
-        LocalDate hoje = LocalDate.now();
-        long dias = ChronoUnit.DAYS.between(dataDeAbertura, hoje);
-        return super.getSaldo() + (0.05 * dias);
+        long diasDesdePrimeiroDeposito = ChronoUnit.DAYS.between(this.dataDeAbertura, LocalDateTime.now());
+        return super.getSaldo() * Math.pow(1.05, diasDesdePrimeiroDeposito);
+    }
+
+    public LocalDateTime getDataDeAbertura() {
+        return dataDeAbertura;
+    }
+
+    public void setDataDeAbertura(LocalDateTime dataDeAbertura) {
+        this.dataDeAbertura = dataDeAbertura;
     }
 }
