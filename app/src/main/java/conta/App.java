22d3341a33a9a758ca7dfbc9bca215b1a871d88f
jonasdspereira <jnsprr@outlook.com>
2024-04-15 09:13:13 -0400

    fix: remocao de duplicacao de codigo e teste da conta corrente

--- app/src/main/java/conta/App.java
+++ app/src/main/java/conta/App.java
@@ -6,12 +6,26 @@ import conta.models.Conta;
 public class App {
 
     public static void main(String[] args) {
-        Usuario usuario = new Usuario();
-        usuario.criarConta("investimento", 1000.0);
-        Conta contaInvestimento = usuario.getConta("investimento");
-        contaInvestimento.sacar(900.0);
 
-        System.out.println("Saldo: " + contaInvestimento.getSaldo());
+        // TESTE CONTA CORRENTE
+
+         Usuario usuario = new Usuario();
+         usuario.criarConta("corrente", 0.0);
+         Conta contaCorrente = usuario.getConta("corrente");
+         System.out.println("Saldo inicial: " + contaCorrente.getSaldo());
+         contaCorrente.depositar(500.0);
+         System.out.println("Saldo após depósito de R$ 500.00: " +
+         contaCorrente.getSaldo());
+         contaCorrente.sacar(200.0);
+         System.out.println("Saldo após saque de R$ 200.00: " +
+         contaCorrente.getSaldo());
+         try {
+         contaCorrente.sacar(2000.0);
+         } catch (RuntimeException ex) {
+         System.out.println(ex.getMessage());
+         }
+         usuario.fecharConta("corrente");
+         System.out.println("Conta corrente fechada com sucesso.");
 
     }
 }
